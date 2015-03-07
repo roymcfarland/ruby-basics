8476c711563cc306c25db9154260dd2379dcfe45
Roy McFarland <roy.mcfarland@gmail.com>
2015-03-07 16:52:43 -0700

    Using ruby strings

--- hello.rb
+++ hello.rb
@@ -6,3 +6,15 @@ puts "Hello #{name}!"
 
 
 # RUBY STRINGS
+
+name = "Roy"
+puts "Hello #{name}" # must use double quotes for interpolation
+puts %Q(Hello #{name}) # %Q adds interpolation
+
+string = <<-STRING
+Hello
+My name is #{name}
+This is my first foray into ruby.
+STRING
+
+puts string
