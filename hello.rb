c8968f21975efa39dd3df73aa853f396b4e6fc3d
Roy McFarland <roy.mcfarland@gmail.com>
2015-03-09 11:30:27 -0600

    Updated strings

--- hello.rb
+++ hello.rb
@@ -1,20 +1,31 @@
-# HOW RUBY WORKS
+#######  HOW RUBY WORKS  #######
 
-print "Please enter your name: "
-name = gets
-puts "Hello #{name}!"
+##
+# print "Please enter your name: "
+# name = gets
+# puts "Hello #{name}!"
 
 
-# RUBY STRINGS
+#######  RUBY STRINGS  #######
 
-name = "Roy"
-puts "Hello #{name}" # must use double quotes for interpolation
-puts %Q(Hello #{name}) # %Q adds interpolation
+##
+# name = "Roy"
+# puts "Hello #{name}" # must use double quotes for interpolation
+# puts %Q(Hello #{name}) # %Q adds interpolation
 
-string = <<-STRING
-Hello
-My name is #{name}
-This is my first foray into ruby.
-STRING
+##
+# string = <<-HELLO
+# Hello
+# My name is #{name}
+# This is my first foray into ruby.
+# HELLO
+# puts string
 
-puts string
+##
+# name = "Roy"
+# string = "Hello!\n\tMy name is #{name}.\n\t\tWorkspaces is fun!"
+# puts string
+# \n = insert new line
+# \t = insert tab
+# \s = insert space
+
